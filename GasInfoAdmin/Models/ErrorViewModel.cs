a2654951d39be5c617378245d6a16c1720f26393
gsdj <gsdj74@gmail.com>
2022-01-16 23:01:28 +0500

    has been added GasInfoAdmin

---
+++ GasInfoAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GasInfoAdmin.Models
+{
+   public class ErrorViewModel
+   {
+      public string RequestId { get; set; }
+
+      public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+   }
+}
