511550f274f82b931cf537bf723edd6cc205931f
Озорнин Юрий < @bank>
2022-02-11 17:02:20 +0500

    added model CalcSteam

---
+++ Business/Interfaces/Calculations/ICalcCharacteristicsSteam.cs
@@ -0,0 +1,20 @@
+﻿using Business.DTO.Characteristics;
+using DataAccess.Entities.Characteristics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces.Calculations
+{
+   public interface ICalcCharacteristicsSteam
+   {
+      Dictionary<int,SteamCharacteristicsDTO> CalcEntities(IEnumerable<SteamCharacteristics> stCs);
+      SteamCharacteristicsDTO CalcEntity(SteamCharacteristics stC);
+      decimal PPa(decimal pmm);
+      decimal PKg(decimal pgm);
+      decimal FKg(decimal ptopp);
+      decimal Rh(decimal fkg, decimal pkg);
+   }
+}
