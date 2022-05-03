f3d9dc3ee85de359844628bf57cc554342b875f8
gsdj <gsdj74@gmail.com>
2022-05-03 15:59:18 +0500

    fix ConsumptionDg on InfoSheet

--- Business/DTO/InfoSheet/ConsumptionDg.cs
+++ Business/DTO/InfoSheet/ConsumptionDg.cs
@@ -1,18 +1,13 @@
-﻿using Business.DTO.Consumption;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Business.DTO.General;
 
 namespace Business.DTO.InfoSheet
 {
    public class ConsumptionDg
    {
       //Расход доменного газа на обогрев (Оценочный, приведение "М-К")
-      public ConsumptionKc1<decimal> Month { get; set; } // ст.м³ с начала месяца
-      public ConsumptionKc1<decimal> Day { get; set; } // ст.м³ (1000кКал/м³)
-      public ConsumptionKc1<decimal> Hour { get; set; } // ст.м³/час
-      public ConsumptionKc1<decimal> UdFv { get; set; } // Удельный, ст.м³/тн. шихты в ф.в.
+      public CbKc Month { get; set; } // ст.м³ с начала месяца
+      public CbKc Day { get; set; } // ст.м³ (1000кКал/м³)
+      public CbKc Hour { get; set; } // ст.м³/час
+      public CbKc UdFv { get; set; } // Удельный, ст.м³/тн. шихты в ф.в.
    }
 }
