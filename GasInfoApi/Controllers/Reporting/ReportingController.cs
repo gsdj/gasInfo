576fa85f38da55cd55c1c9cf2056c63f2be373a8
gsdj <gsdj74@gmail.com>
2022-11-11 13:54:34 +0500

    added api web client

--- GasInfoApi/Controllers/Reporting/ReportingController.cs
+++ GasInfoApi/Controllers/Reporting/ReportingController.cs
@@ -1,6 +1,8 @@
 ﻿using GasInfoApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GasInfoApi.Controllers.Reporting
 {
@@ -25,5 +27,64 @@ namespace GasInfoApi.Controllers.Reporting
 
          return res;
       }
+
+      [HttpGet("Test")]
+      public TestViewModel GetTest()
+      {
+         List<Test> t = new List<Test>
+         {
+            new Test { Id = 1, Date = DateTime.Now, Cb1 = 12.4m, Cb2 = 13.4m, Cb3 = 14.4m, Cb4 = 15.4m, },
+            new Test { Id = 2, Date = DateTime.Now, Cb1 = 41.4m, Cb2 = 12.4m, Cb3 = 13.4m, Cb4 = 14.4m, }
+         };
+
+         List<HeaderTh> h1 = new List<HeaderTh>
+         {
+            new HeaderTh { RowSpan = 1, ColSpan = 1, Name = "Дата" },
+            new HeaderTh { RowSpan = 1, ColSpan = 2, Name = "Кц1" },
+            new HeaderTh { RowSpan = 1, ColSpan = 2, Name = "Кц2" },
+         };
+
+         List<HeaderTh> h2 = new List<HeaderTh>
+         {
+            new HeaderTh { RowSpan = 1, ColSpan = 1, Name = "Кб1" },
+            new HeaderTh { RowSpan = 1, ColSpan = 1, Name = "Кб2" },
+            new HeaderTh { RowSpan = 1, ColSpan = 1, Name = "Кб3" },
+            new HeaderTh { RowSpan = 1, ColSpan = 1, Name = "Кб4" },
+         };
+
+
+         TestViewModel tvm = new TestViewModel
+         {
+            Names = new List<IEnumerable<HeaderTh>> { h1, h2 },
+            TestList = t,
+         };
+
+         return tvm;
+      }
    }
+
+   public class Test
+   {
+      public int Id { get; set; }
+      public DateTime Date { get; set; }
+      public decimal Cb1 { get; set; }
+      public decimal Cb2 { get; set; }
+      public decimal Cb3 { get; set; }
+      public decimal Cb4 { get; set; }
+   }
+
+   public class HeaderTh
+   {
+      public int RowSpan { get; set; }
+      public int ColSpan { get; set; }
+      public string Name { get; set; }
+
+   }
+
+   public class TestViewModel
+   {
+      public IEnumerable<IEnumerable<HeaderTh>> Names { get; set; }
+      public IEnumerable<Test> TestList { get; set; }
+   }
+
 }
---
+++ GasInfoApiClients/gas_info_api_web_client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      sdf
+    </div>
+  );
+}
+
+export default App;
